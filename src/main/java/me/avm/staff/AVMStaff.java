3790fce856a26f711fead2d500eae6b847c8df48
nars-store <sunardiskin@gmail.com>
2026-06-12 16:02:29 +0800

    Add staff manager registration

--- src/main/java/me/avm/staff/AVMStaff.java
+++ src/main/java/me/avm/staff/AVMStaff.java
@@ -3,9 +3,17 @@ package me.avm.staff;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AVMStaff extends JavaPlugin {
+    private StaffManager staffManager;
+
     @Override
     public void onEnable() {
+        this.staffManager = new StaffManager(this);
         getCommand("create").setExecutor(new CreateCommand());
+        getServer().getPluginManager().registerEvents(new StaffListener(staffManager), this);
         getLogger().info("AVMStaff enabled");
+    }
+
+    public StaffManager getStaffManager() {
+        return staffManager;
     }
 }
